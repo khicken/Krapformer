3c1c706ceeac05f8834c396c1ecc1b87f5537603
kale <kalebkim1939@gmail.com>
2020-06-05 22:47:17 -0700

    created object class, slightly buggy
    adding cross compatability

--- src/Main.h
+++ src/Main.h
@@ -1,6 +1,22 @@
 // file with all extensions and shtuff
 #pragma once
 
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+// os 0 for windows, 1 for mac, 2 for linux
+#if defined(__WIN32) || defined(_WIN64)
+#include <tchar.h>
+#include <windows.h>
+const int OS = 0;
+#elif defined(__APPLE__) || defined(__MACH__)
+const int OS = 1;
+#elif defined(__linux__)
+const int OS = 2;
+#endif
+
 #define GLEW_STATIC
 #include "../glew-2.1.0/include/GL/glew.h"
 #include "../glfw-3.3.2/include/GLFW/glfw3.h"
@@ -14,12 +30,6 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "../stb/stb_image.h"
 
-#include <iostream>
-#include <string>
-#include <fstream>
-#include <sstream>
-#include <array>
-#include <vector>
-
+#include "Object.h"
 #include "Shader.h"
 #include "Camera.h"
